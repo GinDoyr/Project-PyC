3a3c20821dcb07376eacff58c315f834a0d00275
GDVevo <109394929+GinDoyr@users.noreply.github.com>
2025-03-01 10:56:09 +0300

    little test

--- tester.py
+++ tester.py
@@ -15,6 +15,7 @@ import arcade
 from arcade.gui import UIAnchorLayout, UIBoxLayout, UIFlatButton, UIView
 from arcade.gui.experimental import UIScrollArea
 from arcade.gui.experimental.scroll_area import UIScrollBar
+# import psutil
 import file_mngr.conf_mngr as conf
 
 window = arcade.Window(title="GUIScrollLayout")
@@ -29,6 +30,8 @@ class MyView(UIView):
         root = self.add_widget(UIAnchorLayout())
         content_left = UIAnchorLayout(size_hint=(0.5, 1), x=window.center_x, y=window.center_y)
         root.add(content_left, anchor_x="left", anchor_y="center")
+        self.player1 = None
+        self.player2 = None
 
         # create the list, which should be scrolled vertically
         self.vertical_list = UIBoxLayout(size_hint=(1, 0), space_between=1)
@@ -60,6 +63,26 @@ class MyView(UIView):
         print(event.source.text)
 
     def on_update(self, delta_time):
+        # print(psutil.cpu_percent()) # just a small cpu/memory usage check test with sounds, dont forget to uncomment import psutil
+        # print(psutil.virtual_memory().percent)
+        # player1 = arcade.Sound
+        # player2 = arcade.Sound
+        # if psutil.cpu_percent() > 90:
+        #     if player1 is arcade.Sound:
+        #         player1 = arcade.sound.play_sound(arcade.load_sound("assets/sounds/hl/siren.wav"))
+        # if psutil.virtual_memory().percent > 76:
+        #     if player2 is arcade.Sound:
+        #         player2 = arcade.sound.play_sound(arcade.load_sound("assets/sounds/hl/bigwarning.wav"))
+        # if player1 is not arcade.Sound:
+        #     if arcade.Sound.is_playing(player1):
+        #         pass
+        #     if arcade.Sound.is_complete(player1):
+        #         player1 = arcade.Sound
+        # if player2 is not arcade.Sound:
+        #     if arcade.Sound.is_playing(player2):
+        #         pass
+        #     if arcade.Sound.is_complete(player2):
+        #         player2 = arcade.Sound
         pass
 
 
