731cd1351bd2662c944e67388eb51a9844d01cf8
GDVevo <109394929+GinDoyr@users.noreply.github.com>
2025-03-07 16:40:54 +0300

    info texts for assel done(?)

--- asset_selector.py
+++ asset_selector.py
@@ -1,8 +1,8 @@
 import arcade
 from arcade.gui import (UIManager, UIBoxLayout, UIDropdown, UIFlatButton, UISlider, UILabel, UITextureButton)
 from arcade.gui.experimental import UIScrollArea
-
-import file_mngr.conf_mngr
+from tkinter.filedialog import askopenfilename
+import shutil
 import file_mngr.conf_mngr as conf
 
 
@@ -62,6 +62,10 @@ class AssetSelector(arcade.View):
         if self.__topright.left+child_width > wid:
             print('OUT THE WINDOW!! if this happened that means i was too lazy to make the rescalable window')
 
+        # top left buttons
+        self.__topleft = self.__ui.add(UIBoxLayout(x=10, y=self.window.height * 15//16, space_between=10, vertical=False))
+        self.__sel_file = self.__topleft.add(UILabel(text='Selected: '))
+
         # bottom right buttons
         self.__botright = self.__ui.add(UIBoxLayout(x=wid * 2 // 3 + (wid // 3 - child_width) // 2, y=10, vertical=False))
         self.__save_button = self.__botright.add(UIFlatButton(width=child_width//2, text='Save changes'))
@@ -75,7 +79,7 @@ class AssetSelector(arcade.View):
         self.__botleft.move(wid*2//3 - child_width*3//2 - 10)
         # dont forget the player for the music! also do the normal resizing and position
 
-        # bottom audio buttons and some ini
+        # audio buttons and some ini
         if call == 'Audio':
             self.__player = None
             self.__cur_sound = None
@@ -87,6 +91,7 @@ class AssetSelector(arcade.View):
             self.__volume_text = self.__volume_btn.add(UILabel(text='Volume: 50%'))
             self.__volume_slider = self.__volume_btn.add(UISlider(value=50, width=100))
             self.__audio_btns.add(self.__volume_btn)
+            self.__volume_len = self.__topleft.add(UILabel(text='Length: '))
 
             @self.__ps_res_btn.event('on_click')
             def on_click(event):
@@ -112,6 +117,28 @@ class AssetSelector(arcade.View):
                 if self.__player is not None:
                     self.__player.volume = self.__volume
                 self.__volume_text.text = f'Volume: {int(self.__volume * 100)}%'
+
+        # scale by y here, i dunno where to put it lol
+        self.__scale_y = round(((self.__topright.bottom - self.__exit_button.height - 30) / self.window.height) * self.window.height) + self.__exit_button.height + 19
+
+        # sprite list
+        if call == 'Sprites':
+            self.__sprite_list = arcade.SpriteList()
+            self.__hb_flag = False
+            spr_path = conf.set_settings('Sprites', 'player_sprite')
+            self.__sprite = arcade.Sprite(spr_path)
+            self.__sel_file.text = f"Selected: {spr_path[spr_path.rfind('/')+1:]}"
+            self.__sprite.position = (self.window.width // 3, (self.__scale_y + self.__exit_button.height + 21) // 2)
+            self.__sprite_list.append(self.__sprite)
+            self.__spr_size = self.__sprite.size
+            self.__spr_resolution = self.__topleft.add(UILabel(text=f'Resolution: {self.__spr_size}'))
+            self.__zoom_txt = self.__topleft.add(UILabel(text='Zoom: '))
+            self.__zoom_slider = self.__topleft.add(UISlider(value=50, width=100))
+
+            @self.__zoom_slider.event("on_change")
+            def on_change(event):
+                print(round(self.__zoom_slider.value/100, 2) + 0.5)
+                self.__sprite.size = (self.__spr_size[0]*(round(self.__zoom_slider.value/100, 2) + 0.5), self.__spr_size[1]*(round(self.__zoom_slider.value/100, 2) + 0.5))
 
         # select box
         self.__vertical_list = UIBoxLayout(size_hint=(1, 0), space_between=1)
@@ -160,14 +187,36 @@ class AssetSelector(arcade.View):
                 self.__pause_bg = False
                 self.__bg_button.text = 'Pause music'
 
-        # sprite list
-        self.__sprite_list = arcade.SpriteList()
-        self.__hb_flag = False
-        self.__scale_y = round(((self.__topright.bottom - self.__exit_button.height - 30) / self.window.height) * self.window.height) + self.__exit_button.height + 19
-        if call == 'Sprites':
-            sprite = arcade.Sprite(conf.set_settings('Sprites', 'player_sprite'))
-            sprite.position = (self.window.width // 3, (self.__scale_y + self.__exit_button.height + 21) // 2)
-            self.__sprite_list.append(sprite)
+        @self.__load.event("on_click")
+        def on_click(event):
+            filename = askopenfilename()
+            try:
+                if not conf.check_path(f'assets/{call.lower()}/{self.__dropdown1.value}/{self.__dropdown2.value}/{filename[filename.rfind("/")+1:]}'):
+                    if call == 'Audio' and filename.endswith(('.mp3', '.wav')):
+                        shutil.copy(filename, f'assets/{call.lower()}/{self.__dropdown1.value}/{self.__dropdown2.value}')
+                        print(f'loaded {filename}')
+                        self.__vertical_list.clear()
+                        for i in conf.return_contents(
+                                f'assets/{self.__call.lower()}/{self.__dropdown1.value.lower()}/{self.__dropdown2.value.lower()}'):
+                            button = UIFlatButton(height=30, size_hint=(1, None), text=f"{i}")
+                            self.__vertical_list.add(button)
+                            button.on_click = self.__selector_click
+
+                    elif call == 'Sprites' and filename.endswith(('.jpg', '.png', '.gif', '.jpeg')):
+                        shutil.copy(filename, f'assets/{call.lower()}/{self.__dropdown1.value}/{self.__dropdown2.value}')
+                        print(f'loaded {filename}')
+                        self.__vertical_list.clear()
+                        for i in conf.return_contents(
+                                f'assets/{self.__call.lower()}/{self.__dropdown1.value.lower()}/{self.__dropdown2.value.lower()}'):
+                            button = UIFlatButton(height=30, size_hint=(1, None), text=f"{i}")
+                            self.__vertical_list.add(button)
+                            button.on_click = self.__selector_click
+                    else:
+                        print(f'incorrect file type! call: {call}; file: {filename}')
+                else:
+                    print(f"already loaded {filename} or closed the window")
+            except Exception as e:
+                print("ERROR! idk what honestly: ", e)
 
         # misc
         self.__call = call
@@ -175,17 +224,36 @@ class AssetSelector(arcade.View):
         self.__previous_option2 = ''
 
     def __selector_click(self, event):
+
         if self.__call == 'Sprites':
             self.__sprite_list.clear()
-            sprite = arcade.Sprite(f'assets/sprites/{self.__dropdown1.value.lower()}/{self.__dropdown2.value.lower()}/{event.source.text}')
-            sprite.position = (self.window.width//3, (self.__scale_y+self.__exit_button.height+21)//2)
-            self.__sprite_list.append(sprite)
+            self.__sprite = arcade.Sprite(f'assets/sprites/{self.__dropdown1.value.lower()}/{self.__dropdown2.value.lower()}/{event.source.text}')
+            self.__sprite.position = (self.window.width//3, (self.__scale_y+self.__exit_button.height+21)//2)
+            self.__sel_file.text = f'Selected: {event.source.text}'
+            self.__spr_size = self.__sprite.size
+            self.__spr_resolution.text = f'Resolution: {self.__spr_size}'
+            self.__zoom_slider.value = 50
+            self.__sprite_list.append(self.__sprite)
         else:
             if self.__player is not None:
                 arcade.stop_sound(self.__player)
             self.__cur_sound = arcade.load_sound(f'assets/audio/{self.__dropdown1.value.lower()}/{self.__dropdown2.value.lower()}/{event.source.text}', streaming=True)
             print(f'launching audio at {self.__volume} volume')
             self.__player = self.__cur_sound.play(volume=self.__volume)
+            self.__sel_file.text = f'Selected: {event.source.text}'
+            sound_len = self.__cur_sound.get_length()
+            ms = round((sound_len-int(sound_len))*100)
+            seconds = int(sound_len//1)
+            minutes = seconds//60
+            seconds -= minutes*60
+            if ms < 10:
+                ms = '0'+str(ms)
+            if seconds < 10:
+                seconds = '0'+str(seconds)
+            if minutes < 10:
+                minutes = '0'+str(minutes)
+            ms, seconds, minutes = str(ms), str(seconds), str(minutes)
+            self.__volume_len.text = f'Length (mm:ss:ms): {minutes}:{seconds}:{ms}'
             self.__ps_res_btn.text = 'Pause'
 
     def on_show_view(self):
@@ -199,13 +267,13 @@ class AssetSelector(arcade.View):
                 arcade.stop_sound(self.__player)
                 self.__player = None
         arcade.stop_sound(self.__curr_audio)
-        self.__curr_audio = None # just to be safe
+        self.__curr_audio = None  # just to be safe
 
     def on_draw(self):
         self.clear()
         arcade.draw_line(self.window.width*2//3, 0, self.window.width*2//3, self.window.height, arcade.color.WHITE, 1)
         arcade.draw_line(0, self.__exit_button.height+21, self.window.width*2//3, self.__exit_button.height+21, arcade.color.WHITE, 1)
-        arcade.draw_line(0, self.__scale_y, self.window.width * 2 // 3, self.__scale_y, arcade.color.WHITE, 1)
+        arcade.draw_line(0, self.__scale_y+1, self.window.width * 2 // 3, self.__scale_y+1, arcade.color.WHITE, 1)
         if self.__call == 'Sprites':
             self.__sprite_list.draw()
             if self.__hb_flag:
@@ -217,8 +285,7 @@ class AssetSelector(arcade.View):
         def on_change(event):
             if self.__previous_option2 != self.__dropdown2.value:
                 self.__vertical_list.clear()
-                for i in conf.return_contents(
-                        f'assets/{self.__call.lower()}/{self.__dropdown1.value.lower()}/{self.__dropdown2.value.lower()}'):
+                for i in conf.return_contents(f'assets/{self.__call.lower()}/{self.__dropdown1.value.lower()}/{self.__dropdown2.value.lower()}'):
                     button = UIFlatButton(height=30, size_hint=(1, None), text=f"{i}")
                     self.__vertical_list.add(button)
                     button.on_click = self.__selector_click
@@ -231,22 +298,6 @@ class AssetSelector(arcade.View):
 
         if not self.__bg_music.is_playing(self.__curr_audio) and not self.__pause_bg:
             self.__curr_audio = self.__bg_music.play(volume=0.05)
-
-        # @self.__load.event("on_click")
-        # # import these before as well
-        # # from tkinter.filedialog import askopenfilename
-        # # import shutil
-        # def on_click(event):
-        #     filename = askopenfilename()
-        #     try:
-        #         if not conf.check_path(f'assets/music/{filename[filename.rfind("/")+1:]}') and (filename.endswith('.mp3') or filename.endswith('.wav')):
-        #             shutil.copy(filename, 'assets/music')
-        #             print('loaded')
-        #         else:
-        #             print("either alr loaded or incorrect file type")
-        #     except Exception as e:
-        #         print("ERROR! idk what honestly: ", e)
-        #     print(filename[filename.rfind("/")+1:], filename)
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.H:
